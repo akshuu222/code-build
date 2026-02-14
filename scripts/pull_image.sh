d8804e35139bb66e4560204752026333663dab0f
akshath <lmakshath123@gmail.com>
2026-02-15 01:46:18 +0530

    removed pull_image.sh file

--- scripts/pull_image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-IMAGE_NAME=924358636787.dkr.ecr.us-east-1.amazonaws.com/prod/node-todo:latest
-
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${REGISTRY_URL}
-echo "Pulling latest image..."
-docker pull $IMAGE_NAME
