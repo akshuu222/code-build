18a69fce65db30489bd2d021126df3b3d721942a
akshath <lmakshath123@gmail.com>
2026-02-15 01:25:57 +0530

    edited in pull_image.sh file

--- scripts/pull_image.sh
+++ scripts/pull_image.sh
@@ -2,5 +2,6 @@
 
 IMAGE_NAME=924358636787.dkr.ecr.us-east-1.amazonaws.com/prod/node-todo:latest
 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${REGISTRY_URL}
 echo "Pulling latest image..."
 docker pull $IMAGE_NAME
