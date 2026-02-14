a58984364f6a8bf17501e7ec5675b4b5d40737e7
akshath <lmakshath123@gmail.com>
2026-02-15 01:14:35 +0530

    edited in pull_image.sh file

--- scripts/pull_image.sh
+++ scripts/pull_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME=akshuu222/web:latest
+IMAGE_NAME=924358636787.dkr.ecr.us-east-1.amazonaws.com/prod/node-todo:latest
 
 echo "Pulling latest image..."
 docker pull $IMAGE_NAME
